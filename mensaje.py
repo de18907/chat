159bd24ec2d2b26f297cfa0bc7f5bd0246e49f11
Sergio Augusto Minaya Herrera <39938438+de18907@users.noreply.github.com>
2020-08-13 23:14:04 -0400

    Add files via upload

--- mensaje.py
+++ mensaje.py
@@ -2,8 +2,8 @@ import os
 import re
 from archivo import archivos
 class mensajes:
-    posicion, posicion2 = [],[] #lista de achivos del chat
-    mensaje1,mensaje2,mensaje5,mensaje4,mensaje3 = [],[],[],[[]],[]
+    posicion = []#lista de achivos del chat
+    mensaje1,mensaje2,mensaje3 = [],[],[]
     for texto in archivos.lista_archivos_chat:
         mirar = open(texto).read()
         if mirar != "" or mirar != " ":
@@ -12,31 +12,25 @@ class mensajes:
                 if palabras[desde] == 'm.' and palabras[desde +1] == '-':
                     posicion.append(desde-3)
     for jsjs in range(len(posicion)):
-        mensaje1.append(palabras[posicion[jsjs-1]:posicion[jsjs]])#aqui se agrega el mensaje por fechas
-    for ver1 in range(len(mensaje1)): #cantidad de mensajes
-        for ver in range(len(mensaje1[ver1])): #cantidad de palabras en un mensaje
-            if re.findall('busc', mensaje1[ver1][ver]):
-                mensaje2.append(ver1)
-    for mm in range(len(mensaje2)-1):
-        if mensaje2[mm] != mensaje2[mm+1]:
-            mensaje3.append(mensaje1[mensaje2[mm+1]])
-    for tt in range(len(mensaje3)):
-        aqui = []
-        for jj in range(len(mensaje3[tt])):
-            if mensaje3[tt][jj] == 'm.' and mensaje3[tt][jj+1] == '-':
-                if mensaje3[tt][jj+2] == '+1' and mensaje3[tt][jj+3] == mensaje3[tt-1][jj+3] and mensaje3[tt][jj+4] == mensaje3[tt-1][jj+4] and mensaje3[tt][jj+5] == mensaje3[tt-1][jj+5]:
-                    aqui.append(tt)
-                elif mensaje3[tt][jj+2] != '+1' and mensaje3[tt][jj+2] == mensaje3[tt-1][jj+2] and mensaje3[tt][jj+3] == mensaje3[tt-1][jj+3]:
-                    aqui.append(tt)
-        if len(aqui) == 1:
-            posicion2.append(aqui[0])
-    agregar = 0
-    for hh in range(len(posicion2)):
-        if posicion2[hh] == posicion2[hh-1]+1:
-            mensaje4[agregar].append(mensaje3[posicion2[hh]])
-        else:
-            mensaje4.append([mensaje3[posicion2[hh]]])
-            agregar += 1
-    for md in range(len(mensaje4)):
-        if mensaje4[md] != []:
-            mensaje5.append(mensaje4[md][0])
+        if palabras[posicion[jsjs-1]:posicion[jsjs]] != []:
+            mensaje1.append(palabras[posicion[jsjs-1]:posicion[jsjs]])#aqui se agrega el mensaje por fechas
+    aqui = -1
+    for ver1 in range(len(mensaje1)):#si el mensaje anterior es de la misma persona entonces este agregamelo al anterior de lo contrario creame un array con el mensaje nuevo
+        for ver2 in range(len(mensaje1[ver1])):
+            if mensaje1[ver1][ver2] == 'm.' and mensaje1[ver1][ver2+1] == '-':
+                if mensaje1[ver1][ver2+2] == '+1' and mensaje1[ver1][ver2+3] == mensaje1[ver1-1][ver2+3] and mensaje1[ver1][ver2+4] == mensaje1[ver1-1][ver2+4]:
+                    mensaje2[aqui].append(' '.join(mensaje1[ver1]))
+                elif mensaje1[ver1][ver2+2] == '+34' and mensaje1[ver1][ver2+3] == mensaje1[ver1-1][ver2+3] and mensaje1[ver1][ver2+4] == mensaje1[ver1-1][ver2+4] and mensaje1[ver1][ver2+5] == mensaje1[ver1-1][ver2+5]:
+                    mensaje2[aqui].append(' '.join(mensaje1[ver1]))
+                elif mensaje1[ver1][ver2+2] != '+1' and mensaje1[ver1][ver2+2] != '+34' and mensaje1[ver1][ver2+2] == mensaje1[ver1-1][ver2+2] and mensaje1[ver1][ver2+3] == mensaje1[ver1-1][ver2+3]:
+                    mensaje2[aqui].append(' '.join(mensaje1[ver1]))
+                else:
+                    mensaje2.append([' '.join(mensaje1[ver1])])
+                    aqui += 1
+    for na in range(len(mensaje2)):#agrega los que dicen busco
+        q = na
+        for an in range(len(mensaje2[na][0].split())):
+            if re.findall('busc', mensaje2[na][0].split()[an]):
+                if na == q:
+                    mensaje3.append(mensaje2[na][0].split())
+                    q = na + 1 
